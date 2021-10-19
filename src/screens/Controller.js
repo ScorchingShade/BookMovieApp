fef2de6bf0be3ff43453295584e481a790dc8734
ScorchingShade <ankushors789@gmail.com>
2021-10-20 04:00:46 +0530

    added the project, up and running now

--- src/screens/Controller.js
+++ src/screens/Controller.js
@@ -1,36 +1,26 @@
-import React from "react";
-import Home from "../screens/home/Home";
-import Details from "../screens/details/Details";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import BookShow from "../screens/bookshow/BookShow";
-import Confirmation from "../screens/confirmation/Confirmation";
-
+import React from 'react'
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
+import Home from "./home/Home"
+import BookShow from "./bookshow/BookShow"
+import Confirmation from "./confirmation/Confirmation"
+import Details from "./details/Details"
+import moviesData from '../common/moviesData'
 const Controller = () => {
-  const baseUrl = "/api/v1/";
-
   return (
     <Router>
-      <div className="main-container">
+      <Switch>
+        <Route exact path="/" render={props => <Home {...props} />} />
         <Route
-          exact
-          path="/"
-          render={(props) => <Home {...props} baseUrl={baseUrl} />}
+          path="/details/:id"
+          component={({ match }) => (
+            <Details match={match} movies={moviesData} />
+          )}
         />
-        <Route
-          path="/movie/:id"
-          render={(props) => <Details {...props} baseUrl={baseUrl} />}
-        />
-        <Route
-          path="/bookshow/:id"
-          render={(props) => <BookShow {...props} baseUrl={baseUrl} />}
-        />
-        <Route
-          path="/confirm/:id"
-          render={(props) => <Confirmation {...props} baseUrl={baseUrl} />}
-        />
-      </div>
+        <Route path="/bookshow/:id" render={props => <BookShow {...props}/>}/>
+        <Route path="/confirm/:id" render={props => <Confirmation {...props}/>}/>
+      </Switch>
     </Router>
-  );
-};
+  )
+}
 
-export default Controller;
+export default Controller
